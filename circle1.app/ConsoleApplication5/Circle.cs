525012f5add04554b4e8f59ec8a1416490a8d32a
munnaju2013 <munnaju2013@gmail.com>
2014-09-29 16:21:17 +0600

    add

---
+++ circle1.app/ConsoleApplication5/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Circle
+    {
+        public double radious;
+
+        public double GetDiameter()
+        {
+            return (2*radious);
+
+        }
+
+        public double GetPerimeter()
+        {
+            return (2*Math.PI*radious);
+        }
+
+        public double GetArea()
+        {
+            return (Math.PI*radious*radious);
+        }
+
+    }
+}
---
+++ circle1.app/ConsoleApplication5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle aCircle = new Circle();
+            Console.Write("Enter Radious : ");
+            aCircle.radious = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Diameter :" + aCircle.GetDiameter() + "\nPerimeter :" + aCircle.GetPerimeter() +
+                              "\nArea: " + aCircle.GetArea());
+            Console.ReadLine();
+
+
+        }
+    }
+}
